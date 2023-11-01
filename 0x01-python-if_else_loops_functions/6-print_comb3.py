8f6f9d43a83a0227f2b1c29088a6acb5c36bd37a
HibaTjr <tajerhiba1@gmail.com>
2023-11-01 12:46:39 -0700

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for tens_digit in range(0, 9):
+    for ones_digit in range(tens_digit + 1, 10):
+        print("{:02d}".format(tens_digit), "{:02d}".format(ones_digit), end=", " if tens_digit < 8 or ones_digit < 9 else "\n")
+
