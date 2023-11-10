9620d388b979204824d50934d22428814366cdcf
HibaTjr <tajerhiba1@gmail.com>
2023-11-10 10:17:59 -0800

    all

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char not in "cC":
+            new_string += char
+    return new_string
+
+if __name__ == "__main__":
+    print(no_c("Best School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
+
